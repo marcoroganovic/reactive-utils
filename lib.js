cd95ec71c16c21e99febee14f45f775034b5308c
Marco Roganovic <marcoroganovic@gmail.com>
2017-01-17 20:06:41 +0100

    second commit

---
+++ lib.js
@@ -0,0 +1,24 @@
+(function(global) {
+  
+  // more to implement
+  //
+  function makeReactive(obj, cb) {
+    return new Proxy(obj, {
+      get: function(target, prop) {
+        return prop in target ? target[prop] : undefined;
+      },
+
+      set: function(target, prop, value) {
+        target[prop] = value;
+        if(cb) cb(value);
+        return target[prop];
+      }
+    });
+  }
+
+
+  global.Reactive = {
+    object: makeReactive
+  }
+
+})(window);
